b5195c22f640834c37c4a2b1c1b5c6d44b4938aa
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2024-02-07 11:31:06 +0100

    delete unused configs

--- lua/mostly_off/config.lua
+++ lua/mostly_off/config.lua
@@ -18,9 +18,6 @@ local defaults = {
     floats = "dark", -- style for floating windows
   },
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
   use_background = true, -- can be light/dark/auto. When auto, background will be set to vim.o.background
 }
